cbe98a1d7e247681f2e15c5fd9340f81f365d4f4
kanhaiya singhal <kanhaiya.singhal@flipkart.com>
2023-04-05 21:37:23 +0530

    correct multithreding

--- verify_IFSC.py
+++ verify_IFSC.py
@@ -1,28 +1,39 @@
 import time
 import requests
 import pandas as pd
+from threading import Lock
 from concurrent.futures import ThreadPoolExecutor
+import threading
 
-value = 1
+untracked_lock = threading.Lock()
+wrong_lock = threading.Lock()
+reRead_data = []
 
+def initializer_worker():
+    raise Exception('Something bad happened!')
 
 def compare_data(new,old):
-    old_obj = ["address","city","district","state","bank_name"]
-    new_obj = ["ADDRESS","CITY","DISTRICT","STATE","BANK"]
+    old_obj = ["address","city","district","state"]
+    new_obj = ["ADDRESS","CITY","DISTRICT","STATE"]
     for i in range(0,len(old_obj)):
         if(old[old_obj[i]] != new[new_obj[i]]):
+                untracked_lock.acquire()
                 wrong_ifsc.append(old["ifsc"])
+                untracked_lock.release()
                 break
 
 def verify_data(data):
-    global value
-    print(value)
-    value += 1
-    response = requests.get(url+data["IFSC"])
-    if(response.status_code != 200):
-        untracked_ifsc.append(data["IFSC"])
-    else:
-        compare_data(data,response.json())
+    try:
+        response = requests.get(url+data["IFSC"])
+        if(response.status_code != 200):
+            untracked_lock.acquire()
+            global untracked_ifsc
+            untracked_ifsc.append(data["IFSC"])
+            untracked_lock.release()
+        else:
+            compare_data(data,response.json())
+    except:
+        reRead_data.append(data)
 
 def create_csv(file_name,headers_list):
     df = pd.DataFrame(list(),columns=headers_list)
@@ -41,12 +52,22 @@ data = []
 for i in range(0,df.index.size):
     data.append(df.iloc[i])
 
+
 print("data extracted")
-with ThreadPoolExecutor(max_workers=500) as pool:
+
+with ThreadPoolExecutor(max_workers=1000) as pool:
     pool.map(verify_data,data)
 
-print(untracked_ifsc)
-print(wrong_ifsc)
+print("done")
+while(len(reRead_data) > 0):
+    temp = reRead_data.copy()
+    reRead_data.clear()
+    print(len(temp))
+    with ThreadPoolExecutor(max_workers=1000) as pool:
+        pool.map(verify_data,temp)
+
+print("untracked ifsc ---->" + str(len(untracked_ifsc)))
+print("wrong ifsc ---->"+ str(len(wrong_ifsc)))
 
 if(len(untracked_ifsc) > 0):
     df = pd.read_csv(untracked_ifsc_file,index_col=0)
@@ -57,6 +78,3 @@ if(len(wrong_ifsc) > 0):
     df = pd.read_csv(wrong_ifsc_file,index_col=0)
     df["IFSC"] = wrong_ifsc
     df.to_csv(wrong_ifsc_file)
-
-
-
